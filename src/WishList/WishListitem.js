eeef89f1d09b2e3477d10489e6284feaa37f55b2
ivanKoretskyy <ivan.koretskyy@eleks.com>
2019-04-15 15:25:14 +0300

    using clone, getSnapshot and apply snapshot for structure

--- src/WishList/WishListitem.js
+++ src/WishList/WishListitem.js
@@ -1,25 +1,56 @@
 import React, { Component} from 'react';
 
 import { observer } from 'mobx-react';
-import { clone } from 'mobx';
+import { clone, getSnapshot, applySnapshot } from 'mobx-state-tree';
 
 class WishItem extends Component {
+  state={
+    edit: false,
+  }
+
+  activeEdit =() =>{
+    this.setState({edit: true, clone: clone(this.props.item)})
+  }
 
   onChangeName(e) {
     this.props.item.changeName(e.target.value)
   }
 
   onChangePrice = (e) => {
-    this.props.item.changePrice(+e.target.value)
+    this.state.clone.changePrice(+e.target.value)
+  }
+
+  save = () => {
+    applySnapshot(this.props.item, getSnapshot(this.state.clone));
+    this.setState({
+      edit: false,
+      clone: null
+    })
+  }
+
+  cancel = () => {
+    this.setState({
+      edit: false,
+      clone: null
+    })
   }
 
   render() { 
     return (
       <div>
         <span>{this.props.item.name}, </span>
-        <input type="text" value={this.props.item.name} onChange={this.onChangeName.bind(this)} />
+        {this.state.edit &&
+          <input type="text" value={clone.name} onChange={this.onChangeName.bind(this)} />
+        }
+        
         <span>{this.props.item.price}</span>
-        <input onChange={this.onChangePrice} value={this.props.item.price} type="number"/>
+        {this.state.edit &&
+        <input onChange={this.onChangePrice} value={this.state.clone.price} type="number"/>
+        }
+        <button onClick={this.activeEdit}>edit</button>
+        <button onClick={this.save}>save</button>
+        <button onClick={this.cancel}>cancel</button>
+        
       </div>
     );
   }
